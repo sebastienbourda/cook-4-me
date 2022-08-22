9f1f7cadea2c894ab6aa42362f37dbb1ed0fd80b
sylvain Dajeans <sylvain.dajeans@gmail.com>
2022-08-22 17:02:57 +0200

    add faker and start seeds of users and offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_132615) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_145316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "offers", force: :cascade do |t|
     t.string "title"
-    t.string "type"
     t.string "price_per_person"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "> Delete Users"
+User.destroy_all
+puts "> Users deleted"
+
+puts "> Create Chef Simon user"
+chef_simon = User.new(
+  email: "chefsimon@grandchef.com",
+  password: "secret"
+)
+chef_simon.save!
+puts "> Chef Simon created"
+
+puts "> Delete Offers"
+Offer.destroy_all
+puts "> Offers deleted"
+
+puts "> Create offers for Chef Simon"
+
+4.times do
+  offer = Offer.new(
+    title: Faker::Restaurant.type,
+    category: Faker::Food.ethnic_category,
+    price_per_person: rand(10..15)
+  )
+  offer.user = chef_simon
+  offer.save!
+  puts "#{offer.title} created"
+end
+
+puts "> offers created"
+puts "> LOG WITH: chefsimon@grandchef.com    / password: secret"
