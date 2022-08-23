033305c8ea21af324d2ac1d5a70a367d622ad653
sylvain Dajeans <sylvain.dajeans@gmail.com>
2022-08-23 16:40:45 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_131647) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_131623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,7 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_131647) do
     t.string "last_name"
     t.string "photo_url"
     t.boolean "chef"
-    t.string "chef_location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
