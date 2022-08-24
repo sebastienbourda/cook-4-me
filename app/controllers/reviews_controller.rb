9af14ad33c07a3cd6be5699eebcb6ce420652827
sylvain Dajeans <sylvain.dajeans@gmail.com>
2022-08-24 12:27:59 +0200

    add review creation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:rating, :content)
+    params.require(:review).permit(:rating, :comment)
   end
 end
