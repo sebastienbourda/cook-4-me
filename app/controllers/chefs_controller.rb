92ec3e5a072a22e48907d044b7cdce298e9729c9
sylvain Dajeans <sylvain.dajeans@gmail.com>
2022-08-25 12:58:22 +0200

    code controller chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,6 +4,19 @@ class ChefsController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(chef_params)
+    @user.chef = true
+    if @user.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def chef_params
+    params.require(:user).permit(:chef_comment, :radius)
   end
 end
