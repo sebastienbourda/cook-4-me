23834af6ecd95759bf18083d2eee6883a74a798a
Thomas Ben <46992388+ThomBen@users.noreply.github.com>
2022-08-25 16:33:02 +0200

    offer create done

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -13,8 +13,22 @@ class OffersController < ApplicationController
   end
 
   def create
-    @offer = Offer.new(offer_params)
+    # @offer_meal = OfferMeal.new
+    # @offer = Offer.new(title:            offer_params[:title],
+    #                    category:         offer_params[:category],
+    #                    price_per_person: offer_params[:price_per_person],
+    #                    user:             current_user)
+    @offer = Offer.new(offer_params.except(:meal_ids).merge(user: current_user))
+    # @offer << offer_params[5]
     if @offer.save
+      meals = offer_params[:meal_ids]
+      meals.delete_at(0)
+      meals.each do |meal_id|
+        offer_meal = OfferMeal.new
+        offer_meal.meal_id = meal_id.to_i
+        offer_meal.offer = @offer
+        offer_meal.save
+      end
       redirect_to dashboard_path
     else
       render :new, status: :unprocessable_entity
@@ -43,6 +57,6 @@ class OffersController < ApplicationController
   private
 
   def offer_params
-    params.require(:offer).permit(:title, :category, :price_per_person, :photo, :meals)
+    params.require(:offer).permit(:title, :category, :price_per_person, :photo, meal_ids: [])
   end
 end
