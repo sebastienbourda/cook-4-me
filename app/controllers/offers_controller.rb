fae4a13e28139f3d74b9b641c06da2f530caf589
Thomas Ben <46992388+ThomBen@users.noreply.github.com>
2022-08-25 14:45:43 +0200

    half done

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -9,17 +9,40 @@ class OffersController < ApplicationController
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    if @offer.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    offer = Offer.find(params[:id])
+    offer.update(offer_params)
+    if offer.save?
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :category, :price_per_person, :photo, :meals)
   end
 end
